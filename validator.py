aebb385080cd73f797fb612a7b9f4c17243dd542
p3rll <scottbusby3@gmail.com>
2017-01-31 08:43:50 -0600

    Fixed ET Bug

--- validator.py
+++ validator.py
@@ -100,111 +100,7 @@ MS = microsoftvulns.MicrosoftVulns()
 SSH = sshvulns.SSHVulns()
 
 
-# All plugins function
-def plugins(host):
-    for issue in host.iter('ReportItem'):
-        port = issue.get('port')
 
-        # SMB Vulnerabilities
-        if issue.get('pluginID') == '57608':  # SMB Signing Disabled
-            SMB.smb_sign_disabled(ipaddress, issue)
-
-            # DNS Vulnerabilities
-        elif issue.get('pluginID') == '12217':  # DNS Server allows cache snooping
-            protocol = issue.get('protocol')
-            DNS.dns_cache_snoop(protocol, ipaddress, port, issue)
-
-            # Microsoft Vulnerabilities
-        elif issue.get('pluginID') == '34477':  # MS08-067
-            MS.ms08_067(ipaddress, port, issue)
-
-            # SSH Vulnerabilities
-        elif issue.get('pluginID') == '90317':  # Weak SSH Algorithms
-            SSH.ssh_weak_algos(ipaddress, port, issue)
-        elif issue.get('pluginID') == '70658':  # CBC Mode Ciphers Enabled
-            SSH.ssh_cbc(ipaddress, port, issue)
-        elif issue.get('pluginID') == '71049':  # Weak MAC Algorithms Enabled
-            SSH.ssh_hmac(ipaddress, port, issue)
-
-            # Misc Vulnerabilities
-        elif issue.get('pluginID') == '25220':  # TCP Timestamp Supported
-            MISC.tcpts_response(ipaddress, issue)
-        elif issue.get('pluginID') == '41028' or issue.get(
-                'pluginID') == '10264':  # SNMP has default community string Public
-            MISC.snmp_default_public(ipaddress, issue)
-        elif issue.get('pluginID') == '11213':  # HTTP TRACE method enabled
-            MISC.http_trace(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '88098':  # Apache ETag Header
-            MISC.http_etag(ipaddress, port, issue, timeout)
-        elif issue.get('svc_name') == 'ntp' and issue.get(
-                'port') == '123':  # Gathers data for all NTP based issues on port 123
-            protocol = issue.get('protocol')
-            MISC.ntp_issues(protocol, ipaddress, port, issue)
-        elif issue.get('svc_name') == 'netbios-ns':  # Gathers data for all netbios-ns based issues
-            MISC.nb_issues(ipaddress, port, issue)
-        elif issue.get('svc_name') == 'cifs':  # Gathers data for all CIFS based issues
-            MISC.cifs_issues(ipaddress, port, issue)
-        elif issue.get('pluginID') == '10079':  # Anonymous FTP Login
-            MISC.ftp_anon(ipaddress, port, issue)
-        elif issue.get('pluginID') == '94437' or issue.get('pluginID') == '26928' or issue.get(
-                'pluginID') == '42873':  # Misc SSL/TLS issues
-            MISC.ssl_cipher_misc(ipaddress, port, issue, timeout)
-
-            # SSL Vulnerabilities
-        elif issue.get('pluginID') == '57582' or issue.get('pluginID') == '45411' or issue.get(
-                'pluginID') == '51192':  # SSL Certificate is Self Signed or untrusted
-            SSL.ssl_self_signed(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '78479' or issue.get(
-                'pluginID') == '80035':  # SSL/TLS Server vulnerable to SSL/TLS POODLE
-            SSL.ssl_poodle(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '35291':  # SSL Certificate uses weak signature algorithms
-            SSL.cert_weak_algor(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '89058':  # SSL Server vulnerable to SSL DROWN
-            SSL.sslv2_DROWN(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '20007':  # SSL Version 2 and/or 3 enabled
-            SSL.ssl_v2v3(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '83738' or issue.get('pluginID') == '83875':  # SSL Server vulnerable to LOGJAM
-            SSL.ssl_logjam(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '81606':  # SSL Server vulnerable to FREAK
-            SSL.ssl_freak(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '65821':  # Server uses RC4 Cipher Suites
-            SSL.rc4_ciphers(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '73412':  # OpenSSL Heartbleed
-            SSL.openssl_heartbleed(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '73412':  # OpenSSL CCS
-            SSL.openssl_CCS(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '62565':  # TLS CRIME
-            SSL.tls_crime(ipaddress, port, issue, timeout)
-
-
-# SSL Plugins function
-def ssl_plugins(host):
-    for issue in host.iter('ReportItem'):
-        port = issue.get('port')
-        if issue.get('pluginID') == '57582' or issue.get('pluginID') == '45411' or issue.get(
-                'pluginID') == '51192':  # SSL Certificate is Self Signed or untrusted
-            SSL.ssl_self_signed(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '78479' or issue.get(
-                'pluginID') == '80035':  # SSL/TLS Server vulnerable to SSL/TLS POODLE
-            SSL.ssl_poodle(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '35291':  # SSL Certificate uses weak signature algorithms
-            SSL.cert_weak_algor(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '89058':  # SSL Server vulnerable to SSL DROWN
-            SSL.sslv2_DROWN(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '20007':  # SSL Version 2 and/or 3 enabled
-            SSL.ssl_v2v3(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '83738' or issue.get('pluginID') == '83875':  # SSL Server vulnerable to LOGJAM
-            SSL.ssl_logjam(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '81606':  # SSL Server vulnerable to FREAK
-            SSL.ssl_freak(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '65821':  # Server uses RC4 Cipher Suites
-            SSL.rc4_ciphers(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '73412':  # OpenSSL Heartbleed
-            SSL.openssl_heartbleed(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '73412':  # OpenSSL CCS
-            SSL.openssl_CCS(ipaddress, port, issue, timeout)
-        elif issue.get('pluginID') == '62565':  # TLS CRIME
-            SSL.tls_crime(ipaddress, port, issue, timeout)
 
 # Remove all items tagged as False Positive
 if args.removefalsepositive:
@@ -239,21 +135,119 @@ if args.file and not args.testssl and not args.timestamp and not args.removeinfo
 
     for host in nessus.iter('ReportHost'):
         ipaddress = host.get('name')
-        plugins(host)
-        # pool = Pool(processes=int(args.thread))  # Create the processor pool, Begin multi-processing
-        # pool.map(plugins,host)
-        # pool.close()  # Close the pool
-        # pool.join()
+        # All plugins
+        for issue in host.iter('ReportItem'):
+            port = issue.get('port')
+
+            # SMB Vulnerabilities
+            if issue.get('pluginID') == '57608':  # SMB Signing Disabled
+                SMB.smb_sign_disabled(ipaddress, issue)
+
+                # DNS Vulnerabilities
+            elif issue.get('pluginID') == '12217':  # DNS Server allows cache snooping
+                protocol = issue.get('protocol')
+                DNS.dns_cache_snoop(protocol, ipaddress, port, issue)
+
+                # Microsoft Vulnerabilities
+            elif issue.get('pluginID') == '34477':  # MS08-067
+                MS.ms08_067(ipaddress, port, issue)
+
+                # SSH Vulnerabilities
+            elif issue.get('pluginID') == '90317':  # Weak SSH Algorithms
+                SSH.ssh_weak_algos(ipaddress, port, issue)
+            elif issue.get('pluginID') == '70658':  # CBC Mode Ciphers Enabled
+                SSH.ssh_cbc(ipaddress, port, issue)
+            elif issue.get('pluginID') == '71049':  # Weak MAC Algorithms Enabled
+                SSH.ssh_hmac(ipaddress, port, issue)
+
+                # Misc Vulnerabilities
+            elif issue.get('pluginID') == '25220':  # TCP Timestamp Supported
+                MISC.tcpts_response(ipaddress, issue)
+            elif issue.get('pluginID') == '41028' or issue.get(
+                    'pluginID') == '10264':  # SNMP has default community string Public
+                MISC.snmp_default_public(ipaddress, issue)
+            elif issue.get('pluginID') == '11213':  # HTTP TRACE method enabled
+                MISC.http_trace(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '88098':  # Apache ETag Header
+                MISC.http_etag(ipaddress, port, issue, timeout)
+            elif issue.get('svc_name') == 'ntp' and issue.get(
+                    'port') == '123':  # Gathers data for all NTP based issues on port 123
+                protocol = issue.get('protocol')
+                MISC.ntp_issues(protocol, ipaddress, port, issue)
+            elif issue.get('svc_name') == 'netbios-ns':  # Gathers data for all netbios-ns based issues
+                MISC.nb_issues(ipaddress, port, issue)
+            elif issue.get('svc_name') == 'cifs':  # Gathers data for all CIFS based issues
+                MISC.cifs_issues(ipaddress, port, issue)
+            elif issue.get('pluginID') == '10079':  # Anonymous FTP Login
+                MISC.ftp_anon(ipaddress, port, issue)
+            elif issue.get('pluginID') == '94437' or issue.get('pluginID') == '26928' or issue.get(
+                    'pluginID') == '42873':  # Misc SSL/TLS issues
+                MISC.ssl_cipher_misc(ipaddress, port, issue, timeout)
+
+                # SSL Vulnerabilities
+            elif issue.get('pluginID') == '57582' or issue.get('pluginID') == '45411' or issue.get(
+                    'pluginID') == '51192':  # SSL Certificate is Self Signed or untrusted
+                SSL.ssl_self_signed(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '78479' or issue.get(
+                    'pluginID') == '80035':  # SSL/TLS Server vulnerable to SSL/TLS POODLE
+                SSL.ssl_poodle(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '35291':  # SSL Certificate uses weak signature algorithms
+                SSL.cert_weak_algor(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '89058':  # SSL Server vulnerable to SSL DROWN
+                SSL.sslv2_DROWN(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '20007':  # SSL Version 2 and/or 3 enabled
+                SSL.ssl_v2v3(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '83738' or issue.get(
+                    'pluginID') == '83875':  # SSL Server vulnerable to LOGJAM
+                SSL.ssl_logjam(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '81606':  # SSL Server vulnerable to FREAK
+                SSL.ssl_freak(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '65821':  # Server uses RC4 Cipher Suites
+                SSL.rc4_ciphers(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '73412':  # OpenSSL Heartbleed
+                SSL.openssl_heartbleed(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '73412':  # OpenSSL CCS
+                SSL.openssl_CCS(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '62565':  # TLS CRIME
+                SSL.tls_crime(ipaddress, port, issue, timeout)
+
 
 
 # Only validate SSL Vulnerabilities
 elif args.file and args.testssl:
     for host in nessus.iter('ReportHost'):
-        ipaddress = host.get('name')
-        # pool = Pool(processes=int(args.thread))  # Create the processor pool
-        # pool.map(ssl_plugins, host)
-        # pool.close()  # Close the pool
-        # pool.join()
+        for issue in host.iter('ReportItem'):
+            ipaddress = host.get('name')
+            port = issue.get('port')
+            if issue.get('pluginID') == '57582' or issue.get('pluginID') == '45411' or issue.get(
+                    'pluginID') == '51192':  # SSL Certificate is Self Signed or untrusted
+                SSL.ssl_self_signed(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '78479' or issue.get(
+                    'pluginID') == '80035':  # SSL/TLS Server vulnerable to SSL/TLS POODLE
+                SSL.ssl_poodle(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '35291':  # SSL Certificate uses weak signature algorithms
+                SSL.cert_weak_algor(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '89058':  # SSL Server vulnerable to SSL DROWN
+                SSL.sslv2_DROWN(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '20007':  # SSL Version 2 and/or 3 enabled
+                SSL.ssl_v2v3(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '83738' or issue.get(
+                    'pluginID') == '83875':  # SSL Server vulnerable to LOGJAM
+                SSL.ssl_logjam(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '81606':  # SSL Server vulnerable to FREAK
+                SSL.ssl_freak(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '65821':  # Server uses RC4 Cipher Suites
+                SSL.rc4_ciphers(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '73412':  # OpenSSL Heartbleed
+                SSL.openssl_heartbleed(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '73412':  # OpenSSL CCS
+                SSL.openssl_CCS(ipaddress, port, issue, timeout)
+            elif issue.get('pluginID') == '62565':  # TLS CRIME
+                SSL.tls_crime(ipaddress, port, issue, timeout)
+
+
+
+
 
 # Only test TCP Timestamp Responses
 elif args.file and args.timestamp:
