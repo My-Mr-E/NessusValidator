04e9b4eeca8423a5a1399f1e5a1049cf01bb2b5f
p3rll <p3rll@users.noreply.github.com>
2017-01-31 11:36:54 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import subprocess
 
 print "Downloading TestSSL.sh"
-testssl = "git clone https://github.com/drwetter/testssl.sh.git"
+testssl = "git clone https://github.com/p3rll/testssl.sh.git"
 command = subprocess.Popen(testssl, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 output,err = command.communicate()
 
