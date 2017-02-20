8b66d59eb378ab3dd3e08875dda52195b02d8a09
p3rll <scottbusby3@gmail.com>
2017-02-20 12:45:44 -0600

    Fixed issues with checking testssl version

--- setup.py
+++ setup.py
@@ -1,51 +1,53 @@
 import subprocess, re, os
+from modules.helper import bcolors
 
 nmap_version = '7.40'
 testssl_version = '2.8'
 
 def nmapUpdate(nmap_version):
     nmap_version_pattern = re.compile(r"Nmap\sversion\s([0-9]{1}.[0-9]{1,2})")
-    print "Checking NMAP Version"
+    print bcolors.OKBLUE + bcolors.BOLD + "Checking NMAP Version" + bcolors.ENDC
     nmap_check = "nmap --version"
     nmap_update = "apt-get install nmap -Y"
     command = subprocess.Popen(nmap_check, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     output,err = command.communicate()
     nmap_match = re.findall(nmap_version_pattern, str(output))
-    print "Current Version: " + nmap_match[0]
+    print bcolors.OKBLUE + bcolors.BOLD + "Current Version: " + nmap_match[0] + bcolors.ENDC
     if nmap_match[0] >= nmap_version:
-        print "NMAP is up to date"
+        print bcolors.OKGREEN + bcolors.BOLD + "NMAP is up to date" + bcolors.ENDC
     else:
-        print "Updating NMAP..."
+        print bcolors.OKBLUE + bcolors.BOLD + "Updating NMAP..." + bcolors.ENDC
         command = subprocess.Popen(nmap_update, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output, err = command.communicate()
         print output
 
 def systemUpdate():
-    print "Updating System..."
-    system_update = "apt-get update -Y"
-    system_upgrade = "apt-get upgrade -Y"
-    command = subprocess.Popen(system_update, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output, err = command.communicate()
-    print output
-    command = subprocess.Popen(system_upgrade, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output1, err = command.communicate()
-    print output1
-    print "Upgrades Completed..."
+    print bcolors.OKBLUE + bcolors.BOLD + "Updating System..." + bcolors.ENDC
+    os.system("apt-get update && apt-get upgrade")
+    print bcolors.OKGREEN + bcolors.BOLD + "Upgrades Completed..." + bcolors.ENDC
 
 def testSSL(testssl_version):
     testssl_version_pattern = re.compile(r"Features\sin\s\[([0-9]{1}.[0-9]{1,2})")
-    print "Checking TestSSL Version"
-    testssl_check = "cat testssl.sh/Readme.md |grep Features"
-    command = subprocess.Popen(testssl_check, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output,err = command.communicate()
-    testssl_match = re.findall(testssl_version_pattern, str(output))
-    print "Current Version: " + testssl_match[0]
-    if testssl_match[0] == testssl_version:
-        print "Correct TestSSL Version exists..."
-    else:
-        print "Downloading Correct Version of TestSSL..."
+    print bcolors.OKBLUE + bcolors.BOLD + 'Checking for TestSSL...' + bcolors.ENDC
+    testssl_exist = os.system('ls|grep testssl.sh|wc -l')
+    if testssl_exist == 0:
+        print bcolors.OKBLUE + bcolors.BOLD + "Downloading Correct Version of TestSSL..." + bcolors.ENDC
         os.system("git clone https://github.com/p3rll/testssl.sh.git")
-        print "TestSSL Download Completed..."
+        print bcolors.OKGREEN + bcolors.BOLD + "TestSSL Download Completed..." + bcolors.ENDC
+    else:
+        print bcolors.OKBLUE + bcolors.BOLD + "Checking TestSSL Version" + bcolors.ENDC
+        testssl_check = "cat testssl.sh/Readme.md |grep Features"
+        command = subprocess.Popen(testssl_check, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output, err = command.communicate()
+        testssl_match = re.findall(testssl_version_pattern, str(output))
+        print bcolors.OKBLUE + bcolors.BOLD + "Current Version: " + testssl_match[0] + bcolors.ENDC
+        if testssl_match[0] == testssl_version:
+            print bcolors.OKGREEN + bcolors.BOLD + "Correct TestSSL Version exists..." + bcolors.ENDC
+        else:
+            print bcolors.OKBLUE + bcolors.BOLD + "Downloading Correct Version of TestSSL..." + bcolors.ENDC
+            os.system("git clone https://github.com/p3rll/testssl.sh.git")
+            print "TestSSL Download Completed..."
+
 
 def checkEnum4Linux():
     enum4linux_pattern = re.compile(r"enum4linux")
@@ -54,17 +56,17 @@ def checkEnum4Linux():
     output,err = command.communicate()
     enum4linux_match = re.findall(enum4linux_pattern, str(output))
     if enum4linux_match:
-        print "Enum4linux was found"
+        print bcolors.OKGREEN + bcolors.BOLD + "Enum4linux was found" + bcolors.ENDC
     else:
-        print "Please install Enum4linux"
+        print bcolors.WARNING + bcolors.BOLD + "Please install Enum4linux" + bcolors.ENDC
 
 def rdp_sec_check():
     print "Downloading rdp-sec-check"
-    os.system('https://github.com/p3rll/rdp-sec-check.git')
-    print 'Download Completed...'
-    print 'Installing requirements...'
+    os.system('git clone https://github.com/p3rll/rdp-sec-check.git')
+    print bcolors.OKGREEN + bcolors.BOLD + 'Download Completed...' + bcolors.ENDC
+    print bcolors.OKBLUE + bcolors.BOLD + 'Installing requirements...' + bcolors.ENDC
     os.system('cpan install Encoding::BER')
-    print 'rdp-sec-check setup completed.'
+    print bcolors.OKGREEN + bcolors.BOLD + 'rdp-sec-check setup completed.' + bcolors.ENDC
 
 
 systemUpdate()
@@ -73,6 +75,6 @@ nmapUpdate(nmap_version)
 testSSL(testssl_version)
 checkEnum4Linux()
 print ""
-print "Please report bugs via Github."
-print "Thanks for using Validator!"
+print bcolors.OKGREEN + bcolors.BOLD + "Please report bugs via Github." + bcolors.ENDC
+print bcolors.OKGREEN + bcolors.BOLD + "Thanks for using Validator!" + bcolors.ENDC
 
